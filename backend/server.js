d7263c90c7ae0e5ef3da35679a943500a305a6c4
Vivek Singh <vivek280197@gmail.com>
2024-12-31 09:08:01 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,21 @@ connectCloudinary();
 
 //middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = [
+  'https://yourfrontend.vercel.app', 
+  'https://youradmin.vercel.app'
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // Allow request
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+}));
 
 //api endpoints
 app.use("/api/user", userRouter);
