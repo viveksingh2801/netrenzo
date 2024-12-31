84205ceb9db2bcd3424332d231e48ef2a379b150
Vivek Singh <vivek280197@gmail.com>
2024-12-31 08:23:20 +0530

    first commit

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -30,17 +30,21 @@ const Orders = ({ token }) => {
     }
   };
 
-   const statusHandler = async (event, orderId) => {
-     try {
-      const response = await axios.post(backendUrl + "/api/order/status", {orderId, status:event.target.value}, {headers: {token}})
-      if(response.data.success) {
-        await fetchAllOrders()
-      } 
-    } catch(error) {
-      console.log(error)
-      toast.error(response.data.message)
+  const statusHandler = async (event, orderId) => {
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/order/status",
+        { orderId, status: event.target.value },
+        { headers: { token } }
+      );
+      if (response.data.success) {
+        await fetchAllOrders();
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(response.data.message);
     }
-   }
+  };
 
   useEffect(() => {
     fetchAllOrders();
@@ -51,7 +55,10 @@ const Orders = ({ token }) => {
 
       <div>
         {orders.map((order, index) => (
-          <div className="grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700" key={index}>
+          <div
+            className="grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700"
+            key={index}
+          >
             <img className="w-12" src={assets.parcel_icon} alt="" />
             <div>
               <div>
@@ -71,7 +78,9 @@ const Orders = ({ token }) => {
                   }
                 })}
               </div>
-              <p className="mt-3 mb-2 font-medium">{order.address.firstName + " " + order.address.lastName}</p>
+              <p className="mt-3 mb-2 font-medium">
+                {order.address.firstName + " " + order.address.lastName}
+              </p>
               <div>
                 <p>{order.address.street + ","}</p>
                 <p>
@@ -87,7 +96,9 @@ const Orders = ({ token }) => {
               <p>{order.address.phone}</p>
             </div>
             <div>
-              <p className="text-sm sm:text-[15px]">Items: {order.items.length}</p>
+              <p className="text-sm sm:text-[15px]">
+                Items: {order.items.length}
+              </p>
               <p className="mt-3">Method: {order.paymentMethod}</p>
               <p>Payment: {order.payment ? "Done" : "Pending"}</p>
               <p>Date: {new Date(order.date).toLocaleDateString()}</p>
@@ -96,7 +107,11 @@ const Orders = ({ token }) => {
               {currency}
               {order.amount}
             </p>
-            <select onChange={(event) => statusHandler(event,order._id)} value={order.status} className="p-2 font-semibold">
+            <select
+              onChange={(event) => statusHandler(event, order._id)}
+              value={order.status}
+              className="p-2 font-semibold"
+            >
               <option value="Order Placed">Order Placed</option>
               <option value="Packing">Packing</option>
               <option value="Shipped">Shipped</option>
